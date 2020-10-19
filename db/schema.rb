03386d9f06bbfbf77c89ed5b2e56187713609b18
jrogers-dev <jrogers.dev@gmail.com>
2020-10-18 20:22:32 -0700

    Devise migration successfully run

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_10_19_002744) do
 
+  create_table "properties", force: :cascade do |t|
+    t.integer "landlord_id", null: false
+    t.integer "tenant_id"
+    t.string "address", null: false
+    t.integer "rooms", null: false
+    t.integer "bathrooms", null: false
+    t.integer "squarefeet", null: false
+    t.integer "storeys", null: false
+    t.integer "rent", null: false
+    t.text "description"
+    t.boolean "available", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["landlord_id"], name: "index_properties_on_landlord_id"
+    t.index ["tenant_id"], name: "index_properties_on_tenant_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "properties", "users", column: "landlord_id"
+  add_foreign_key "properties", "users", column: "tenant_id"
 end
