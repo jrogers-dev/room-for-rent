1056e9a69e4c9d1dc7e4760ba5256f3e5e3fd579
jrogers-dev <jrogers.dev@gmail.com>
2020-10-18 17:09:03 -0700

    Added comments to User model to clarify associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+    #The following associations allow a user to "own" or "rent" a property through
+    #foreign keys in the Property class, making them a Landlord or Tenant and 
+    #associating them with other Users through owned or rented properties 
     has_many :owned_properties, class_name: "Property", foreign_key: "landlord_id"
     has_many :tenants, through: :owned_properties
+
     has_many :rented_properties, class_name: "Property", foreign_key: "tenant_id"
     has_many :landlords, through: :rented_properties
 end
