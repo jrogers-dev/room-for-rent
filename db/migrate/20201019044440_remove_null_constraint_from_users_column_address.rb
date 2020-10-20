47825e112d7ab00d6be4600bb8f1ee6816eebc24
jrogers-dev <jrogers.dev@gmail.com>
2020-10-19 17:39:19 -0700

    Added a home controller for landing pages that don't require being logged in. Created an index view for the home controller and set it as the root path. Created a migration to remove the null constraint on the User model name attribute.

---
+++ db/migrate/20201019044440_remove_null_constraint_from_users_column_address.rb
@@ -0,0 +1,5 @@
+class RemoveNullConstraintFromUsersColumnAddress < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null :users, :name, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_19_002744) do
+ActiveRecord::Schema.define(version: 2020_10_19_044440) do
 
   create_table "properties", force: :cascade do |t|
     t.integer "landlord_id", null: false
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2020_10_19_002744) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", default: "", null: false
