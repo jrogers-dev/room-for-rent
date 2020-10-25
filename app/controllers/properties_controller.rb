e247e6fd98364d9061aee8456b07a798516a2053
James Rogers <jarogers095@gmail.com>
2020-10-24 18:03:25 -0700

    Built out the properties controller and views substantially: index, show, new, create and their associated views and functions are now in a working state. Changed belongs_to :tenant to optional on the Property model. Added some initial code to the users show page, and adjusted the links in the dashboard.

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,37 @@
 class PropertiesController < ApplicationController
+    
+    def index
+
+    end
+
+    def show
+        @property = Property.find(params[:id])
+    end
+
+
+    def new
+        @property = Property.new
+    end
+
+    def create
+        params.require(:property).permit!
+        @property = Property.new(params[:property])
+        @property.landlord_id = current_user.id
+        @property.save!
+        redirect_to property_path(@property)
+    end
+
+
+    def edit
+        
+    end
+
+    def update
+
+    end
+
+
+    def destroy
+
+    end
 end
