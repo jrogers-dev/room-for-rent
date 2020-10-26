9501bc5efe664f2b4f36ac10a5b3d25e3bf8123b
James Rogers <jarogers095@gmail.com>
2020-10-25 21:35:04 -0700

    Built out some of the MessagesController with actions index, show, new, and created their respective views. Modified the User Dashboard to include a link button to an inbox. Currently the way to create a new message is through a button in the inbox, for testing purposes. However, the actual functionality will require users to send messages using a button on the property listing they are viewing, so we can have both the sender_id and recipient_id ready.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,15 @@
 class MessagesController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+
+    end
+
+    def show
+        @message = Message.find(params[:id])
+    end
+
+    def new
+        @message = Message.new(sender_id: current_user.id)
+    end
 end
